d71f058f9371153b6e0f6170c2918721c19e59cc
Jemel Barreto <jemelb@usf.edu>
2020-02-19 22:24:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Deliverable2_Iteration_Runowski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please Enter an Integer between 2 and 20.");
+            try
+            {
+                
+                string input = Console.ReadLine();
+                int x = int.Parse(input);
+                int count = 1;
+                if (x > 20 || x < 2)
+                {
+                    Console.WriteLine("Enter number between 2 and 20");
+                }
+                else
+                {
+                    while (count <= x)
+                    {
+                        Console.WriteLine("You have entered " + x + ". This is the current integer value in the loop: " + count);
+                        count++; 
+                    }
+                }
+            }
+            catch
+            {
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
